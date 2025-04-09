5df49ed07a3335229b1660caadaced43e4bbd25b
devoirr <138160506+devoirr@users.noreply.github.com>
2025-04-09 20:59:42 +0300

    Moved LocaleLoader.kt functionality to Messages.kt

--- src/main/kotlin/me/snipz/api/locale/Messages.kt
+++ src/main/kotlin/me/snipz/api/locale/Messages.kt
@@ -1,5 +1,21 @@
 package me.snipz.api.locale
 
+import com.google.gson.Gson
+import java.io.File
+
 data class Messages(private val messages: Map<String, String>) {
     fun getMessage(key: String) = messages[key]
+
+    companion object {
+        fun load(file: File): Messages? {
+            if (!file.exists()) return null
+
+            val json = file.readText()
+            val map =
+                (Gson().fromJson(json, Map::class.java) as Map<*, *>).map { it.key.toString() to it.value.toString() }
+                    .toMap()
+
+            return Messages(map)
+        }
+    }
 }
