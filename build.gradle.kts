4c853034b00bf05062460f1d82feea031079a085
devoirr <138160506+devoirr@users.noreply.github.com>
2025-04-11 14:47:36 +0300

    Added PacketEvents and ORMLite dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,14 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://repo.purpurmc.org/snapshots")
+    maven("https://repo.codemc.io/repository/maven-releases/")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
 }
 
 dependencies {
-    compileOnly("org.purpurmc.purpur", "purpur-api", "1.21.3-R0.1-SNAPSHOT")
+    compileOnly("org.purpurmc.purpur", "purpur-api", "1.21.4-R0.1-SNAPSHOT")
+    implementation("com.j256.ormlite:ormlite-jdbc:6.1")
+    implementation("com.github.retrooper:packetevents-spigot:2.7.0")
 }
 
 kotlin {
