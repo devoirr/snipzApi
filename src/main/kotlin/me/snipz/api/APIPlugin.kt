f63e277c0149e91223652d0558c9762b147702ff
devoirr <138160506+devoirr@users.noreply.github.com>
2025-04-11 23:46:18 +0300

    Added Vault permission service and my service-manager to the main class.

--- src/main/kotlin/me/snipz/api/APIPlugin.kt
+++ src/main/kotlin/me/snipz/api/APIPlugin.kt
@@ -3,8 +3,11 @@ package me.snipz.api
 import com.mojang.brigadier.Command
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import me.snipz.api.cooldown.CooldownService
 import me.snipz.api.gui.GUIListener
 import me.snipz.api.gui.example.ExampleMenu
+import me.snipz.api.service.ServiceManager
+import net.milkbowl.vault.permission.Permission
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -14,10 +17,19 @@ class APIPlugin: JavaPlugin() {
         lateinit var instance: APIPlugin
     }
 
+    lateinit var permission: Permission
+
+    lateinit var serviceManager: ServiceManager
+
     override fun onEnable() {
         instance = this
+        serviceManager = ServiceManager()
+
+        serviceManager.register(CooldownService::class, CooldownService())
 
         GUIListener().register()
+
+        permission = server.servicesManager.getRegistration(Permission::class.java)!!.provider
 
         this.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) { commands ->
             commands.registrar().register(Commands.literal("test").executes { ctx ->
